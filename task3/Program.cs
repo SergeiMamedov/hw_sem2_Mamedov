729fd0f9130f1f55c0a4091826fd12edb51f8628
Sergei <sergeimamedov9976@gmail.com>
2023-01-22 21:35:31 +0600

    третья задача

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите номер дня недели: ");
+string number = Console.ReadLine();
+int dayoff = Convert.ToInt32(number);
+if (dayoff > 5)
+{
+    string[] week = new string [] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+    System.Console.WriteLine($"Выходной");
+}
+else
+{
+    System.Console.WriteLine("рабочий день");
+}
